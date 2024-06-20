62f82d0ae4ec2f983ded7c115ac2327cadf13e61
kevinacostaa <kevinmag999@gmail.com>
2024-06-20 14:23:11 +0000

    2-strlen

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,14 @@
 #include "main.h"
 /**
- * _strlen - returns the length of a string.
- * @s: input string.
- * Return: return 16
- */
+* _strlen - returns the length of a string.
+* @s: input string.
+* Return: return 16
+*/
 int _strlen(char *s)
 {
 	int len;
 
-	for (len = 0; s[len] != '\0'; len++)
-	{
-		
-	}
+	for (len = 0; s[len] != '\0'; len++);
+
 	return (len);
 }
