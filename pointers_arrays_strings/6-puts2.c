c2effcf6c4b61aaf87ffb5aa9455028b18c153bd
kevinacostaa <kevinmag999@gmail.com>
2024-06-25 17:22:42 +0000

    6-puts2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+void puts2(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		if (a % 2 != 0)
+			continue;
+		else
+			_putchar(str[a]);
+	}
+	_putchar('\n');
+}
