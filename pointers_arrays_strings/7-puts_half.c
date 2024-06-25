892c662fa65ef4b916b98ff26572d79635756583
kevinacostaa <kevinmag999@gmail.com>
2024-06-25 17:34:24 +0000

    7-puts_half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * puts_half - print alf
+ *
+ * @str: string
+ *
+ * Return: no return
+ */
+void puts_half(char *str)
+{
+	int l;
+	int n;
+
+	for (l = 0; str[l] != '\0'; l++)
+	{
+	}
+	if (l % 2 == 0)
+	{
+		n = l / 2;
+	}
+	else
+	{
+		n = (l - 1) / 2 + 1;
+	}
+	for (; n < l; n++)
+	{
+		_putchar(str[n]);
+	}
+	_putchar('\n');
+}
