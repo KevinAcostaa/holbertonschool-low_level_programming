d6096df78b0a31e12df521468998dd108b6fe836
kevinacostaa <kevinmag999@gmail.com>
2024-06-25 13:02:45 +0000

    5-rev_string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string - reverse a string
+ *
+ * @s: input string
+ *
+ * Return: no return
+ */
+void rev_string(char *s)
+{
+	int longitud, i;
+	char caracter;
+
+	for (longitud = 0; s[longitud] != '\0'; longitud++)
+	{
+		
+	}
+	for (i=0; i < longitud / 2; i++)
+	{
+		caracter = s[i];
+		s[i]= s[longitud - i - 1];
+		s[longitud - i - 1] = caracter;
+	}
+}
