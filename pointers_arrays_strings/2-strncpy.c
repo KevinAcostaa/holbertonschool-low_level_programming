54632dced241b323343bf92ae5b0271058b33720
kevinacostaa <kevinmag999@gmail.com>
2024-06-26 00:45:31 +0000

    2-strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncpy - function
+ * @dest: string 1
+ * @src: string 2
+ * @n: number caracter
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
