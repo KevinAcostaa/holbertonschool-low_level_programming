a881622f7324472491b17854f249b768d68749d0
kevinacostaa <kevinmag999@gmail.com>
2024-06-28 16:55:02 +0000

    5-sqrt

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * raiz_c - that returns the natural square root of a number
+ * @x: variable
+ * @n: variable
+ * Return: always
+ *
+ */
+int raiz_c(int x, int n)
+{
+	if (x > n)
+		return (-1);
+
+	else if (n != (x * x))
+		x++;
+	else
+		return (x);
+
+	return (raiz_c(x, n));
+}
+int _sqrt_recursion(int n)
+{
+	int x = 1;
+
+	return (raiz_c(x, n));
+}
