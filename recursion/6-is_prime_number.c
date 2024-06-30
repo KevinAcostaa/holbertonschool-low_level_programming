d02e6c8bc7b483dd0d0f18fb7254757c360111c6
kevinacostaa <kevinmag999@gmail.com>
2024-06-30 22:40:28 +0000

    6-is_prime_number

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * is_prime_number - fucntion
+ *
+ * @n: variable
+ *
+ * Return: return (1) or (0)
+ */
+int is_prime_number(int n)
+{
+	if (n % 1 && n % n)
+	{
+		return (1);
+	}
+
+	else if
+	{
+		return (0);
+	}
+}
