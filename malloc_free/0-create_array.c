cd9367dd0d34a0291dc9b712e8d97f216b5b2274
kevinacostaa <kevinmag999@gmail.com>
2024-07-03 19:46:22 +0000

    0-create_array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -16,18 +16,14 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	size--;
 	array = malloc((size + 1) * sizeof(char));
-
 	if (array == NULL)
 	{
 		return (NULL);
 	}
-
        	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
 	}
-	array[size] = '\0';
 	return (array);
 }
