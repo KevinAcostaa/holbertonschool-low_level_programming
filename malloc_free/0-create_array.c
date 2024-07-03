c127d634d838368187ec342c5c448a712ed6a9f8
kevinacostaa <kevinmag999@gmail.com>
2024-07-03 19:36:48 +0000

    0-create_array

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - sasa
+ * @size: assasa
+ * @c: asas
+ *
+ * Return: return array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return NULL;
+	}
+	array = malloc((size + 1) * sizeof(char));
+
+	if (array == NULL)
+	{
+		return NULL;
+	}
+
+       	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+	array[size] = '\0';
+	return array;
+}
