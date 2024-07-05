a722b00ac8ddf35f18a03eedc99cef8287847968
kevinacostaa <kevinmag999@gmail.com>
2024-07-05 14:33:13 +0000

    2-str_concat

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *s3;
+	int long1, long2;
+	int i,j;
+	
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (long1 = 0; s1[long1] != '\0'; long1++)
+	{
+
+	}
+	for (long2 = 0; s2[long2] != '\0'; long2++)
+	{
+		
+	}
+	s3 = malloc((long1 + long2 +1) * sizeof(char));
+
+	if (s3 == 0)
+	{	free(s3);
+		return (NULL);
+	}
+
+	for (i = 0; i < long1; i++)
+	{
+		s3[i] = s1[i];
+	}
+	for (j = 0; j < long2; j++)
+	{
+		s3[i + j] = s2[j];
+	}
+	s3[x] = '\0';
+	return (s3);
+}
