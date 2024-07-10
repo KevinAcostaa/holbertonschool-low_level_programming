4b95d9e978d5e20f53cc9747c9b636b1f75bbfc7
kevinacostaa <kevinmag999@gmail.com>
2024-07-10 12:37:49 +0000

    0-malloc_checked

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * malloc_checked - check the code
+ * @b: entero
+ * Return: Always
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *k;
+
+	k = malloc(b);
+
+	if (k == NULL)
+	{
+		exit(98);
+	}
+	return(k);
+
+
+
+
+
+}
