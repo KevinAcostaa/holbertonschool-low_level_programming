6d03ee64bfc2e97c3d45eb6e5e184d62b89e6fd0
kevinacostaa <kevinmag999@gmail.com>
2024-07-14 15:21:23 +0000

    2-print_dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -11,25 +11,28 @@ void print_dog(struct dog *d)
 	{
 		return;
 	}
-	if (d->name == NULL)
-	{
-		printf("Name: (nill)\n");
-	}
 	else
 	{
-		printf("Name: %s\n", (*d).name);
-	}
 
-	printf("Age: %f\n", (*d).age);
+		if (d->name == NULL)
+		{
+			printf("Name: (nill)\n");
+		}
+		else
+		{
+			printf("Name: %s\n", (*d).name);
+		}
 
-	if (d->owner == NULL)
-	{
-		printf("Owner: (nill)\n");
-	}
-	else
-	{
-		printf("Owner: %s\n", (*d).owner);
-	}
+		printf("Age: %f\n", (*d).age);
 
+		if (d->owner == NULL)
+		{
+			printf("Owner: (nill)\n");
+		}
+		else
+		{
+			printf("Owner: %s\n", (*d).owner);
+		}
+	}
 
 }
