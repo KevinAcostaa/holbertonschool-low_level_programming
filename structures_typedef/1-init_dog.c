1c51f709cc68fd0cbbf0c5f32f3c8721c1004e1e
kevinacostaa <kevinmag999@gmail.com>
2024-07-14 15:00:33 +0000

    1-init_dog

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,3 +1,5 @@
+#include <stddef.h>
+#include "dog.h"
 /**
  * init_dog - struct the dog
  * @d: pointer
@@ -11,7 +13,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	{
 		d->name = name;
 		d->age = age;
-		d->owner = owner;	
+		d->owner = owner;
 	}
 
 }
