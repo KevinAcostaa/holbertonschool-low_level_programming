462dd002d17012f12575067586e99904aeceea80
kevinacostaa <kevinmag999@gmail.com>
2024-07-14 15:43:24 +0000

    3-dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,18 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
-
+#include <stdio.h>
 /**
- * dog_t - Typedef dog
- */
-typedef struct dog dog_t;
-
-/**
- * struct dog - struct
+ * struct dog - dog
+ *
  * @name: name
  * @age: age
  * @owner: owner
- *
- * Description: structura dog
  */
 struct dog
 {
@@ -20,9 +14,21 @@ struct dog
 	float age;
 	char *owner;
 };
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/**
+ * struct dog_t - dog
+ *
+ * @name: name
+ * @age: eage
+ * @owner: owner
+ */
+typedef struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
