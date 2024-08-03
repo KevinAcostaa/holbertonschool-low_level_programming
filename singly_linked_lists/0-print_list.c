97c6d26f5e3040897de2d1b1942249cb8e249ce7
kevinacostaa <kevinmag999@gmail.com>
2024-08-03 17:41:26 +0000

    0-print_list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stddef.h>
 #include "lists.h"
 /**
  * print_list - function
@@ -9,5 +9,17 @@
 
 size_t print_list(const list_t *h)
 {
-	
+	size_t count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
