d0e21955dd6e708b6f9612ffa43b1ed4d16f90d5
kevinacostaa <kevinmag999@gmail.com>
2024-10-24 20:22:09 +0000

    1-alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - function
+ * print_alphabet - imprime alfabeto en min√scula 
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
