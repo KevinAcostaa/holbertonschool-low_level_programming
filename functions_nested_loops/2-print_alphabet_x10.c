220719f4aa0efe8ead1bbc8adfa4aef8cda99c10
kevinacostaa <kevinmag999@gmail.com>
2024-10-24 22:56:25 +0000

    2-alphabet_x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * void print_alphabet_x10(void) - function
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char abc;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (abc = 'a'; abc <= 'z'; abc++)
+		{
+			_putchar(abc);
+		}
+		_putchar('\n');
+	}
+}
