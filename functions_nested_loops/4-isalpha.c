bfbcb67339d199ce9522b9f38c3103f4eda377f7
kevinacostaa <kevinmag999@gmail.com>
2024-10-25 15:05:14 +0000

    4-isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _isalpha - function
+ * @c: character
+ * Return: 1 or 0
+ */
+/**
+ * Otra manera de lograr el mismo objetivo es cambiando el body de la function a:
+ * return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
+ *
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
