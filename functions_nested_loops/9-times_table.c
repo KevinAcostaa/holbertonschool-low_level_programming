529318634032bbefdb6579ec2e109769f4f3d7d5
kevinacostaa <kevinmag999@gmail.com>
2024-10-26 15:41:14 +0000

    9-times_table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -27,8 +27,8 @@ void times_table(void)
 			{
 				_putchar((product / 10) + '0');
 			}
-				_putchar((product % 10) + '0');
+			_putchar((product % 10) + '0');
 		}
-			_putchar('\n');
+		_putchar('\n');
 	}
 }
