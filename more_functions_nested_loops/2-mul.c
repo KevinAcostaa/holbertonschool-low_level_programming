b6bd95389a55a8420ee59a28b42f367359acc002
kevinacostaa <kevinmag999@gmail.com>
2024-10-29 17:03:37 +0000

    2-mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - function
+ * @a: 1er parametro
+ * @b: 2do parametro
+ * Return: Always integers
+ */
+int mul(int a, int b)
+{
+	int multi = a * b;
+
+	return (multi);
+}
