6f049154d8743ba27c21d64af5a5d9e8f2ac006e
kevinacostaa <kevinmag999@gmail.com>
2024-10-30 14:35:06 +0000

    6-lines

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - function
+ *
+ * @n: parametro
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		_putchar('_');
+	}
+	_putchar('\n');
+}
