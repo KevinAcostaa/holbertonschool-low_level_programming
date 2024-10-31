3803ec3f20be3eb5791793a67a99638d1fe31770
Kevinacostaa <kevinmag999@gmail.com>
2024-10-31 17:34:08 +0000

    4-print_rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+void print_rev(char *s)
+{
+	int leng = 0;
+	int i;
+
+	while (s[leng] != '\0')
+	{
+		leng++;
+	}
+	for (i = leng - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
