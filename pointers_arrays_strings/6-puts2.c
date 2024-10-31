06d7b8b55f93d07de4dc2aca7dddd417727af3e3
Kevinacostaa <kevinmag999@gmail.com>
2024-10-31 18:32:37 +0000

    6-puts2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * puts2 - function
  *
@@ -10,7 +11,7 @@ void puts2(char *str)
 
 	for (i = 0; str[i] != '\0'; i += 2)
 	{
-		_putchar(str[i]);
+		putchar(str[i]);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
