a7105c1d77316c94dd4ee610b43f135e0d0e3184
Kevinacostaa <kevinmag999@gmail.com>
2024-10-31 18:02:07 +0000

    5-rev_string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - function
+ *
+ * @s: pointer
+ */
+void rev_string(char *s)
+{
+	int leng = 0;
+	int i;
+
+	while (s[leng] != '\0')
+	{
+		leng++;
+	}
+	for (i = 0; i < leng / 2; i++)
+	{
+		char cadena = s[i];
+		s[i] = s[leng - 1 - i];
+		s[leng - 1 - i] = cadena;
+	}
+}
