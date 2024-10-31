cda7b9dc0431070eb38c15b2b5e74c87b95d20aa
Kevinacostaa <kevinmag999@gmail.com>
2024-10-31 18:07:08 +0000

    5-rev_string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,6 +8,7 @@ void rev_string(char *s)
 {
 	int leng = 0;
 	int i;
+	char cadena;
 
 	while (s[leng] != '\0')
 	{
@@ -15,7 +16,7 @@ void rev_string(char *s)
 	}
 	for (i = 0; i < leng / 2; i++)
 	{
-		char cadena = s[i];
+		cadena = s[i];
 
 		s[i] = s[leng - 1 - i];
 		s[leng - 1 - i] = cadena;
