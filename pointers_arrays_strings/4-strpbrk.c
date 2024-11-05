097d3926c6c6fdc6965a30bf5dbdb0ecc43d0669
kevinacostaa <kevinmag999@gmail.com>
2024-11-05 18:01:15 +0000

    4-strpbrk

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strpbrk - function
+ * @s: string
+ * @accept: string
+ * Return: bytes in s
+ */
+char *_strpbrk(char *s, char *accept)
+{
+
+	while (*s != '\0')
+	{
+		char *p = accept;
+
+		while (*p != '\0')
+		{
+			if(*s == *p)
+			{
+				return (s);
+			}
+			p++;
+		}
+		s++;
+	}
+	return (NULL);
+}
