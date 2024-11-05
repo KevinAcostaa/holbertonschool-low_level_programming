640bc30230e04c3cb5d1498f38d466d7016fa550
kevinacostaa <kevinmag999@gmail.com>
2024-11-05 17:46:22 +0000

    3-strspn

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strspn - function
+ * @s: cadena 1
+ * @accept: cadena 2
+ * Return: longitud
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int contador = 0;
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				contador++;
+				break;
+			}
+		}
+		if (accept[j] == '\0')
+		{
+			break;
+		}
+	}
+	return (contador);
+	
+}
