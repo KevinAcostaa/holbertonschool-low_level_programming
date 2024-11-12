f082eb295765c8784cb6a2c7e9fd86b009515c03
kevinacostaa <kevinmag999@gmail.com>
2024-11-12 18:57:11 +0000

    1-strdup

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - function
+ * @str: cadena
+ * Return: copy
+ */
+char *_strdup(char *str)
+{
+	char *copy;
+	int i, l;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	
+	}
+
+	copy = malloc(i * sizeof(char));
+
+	if (copy == NULL)
+	{
+		return (NULL);
+	}
+
+	for (l = 0; str[l] != '\0'; l++)
+	{
+		copy[l] = str[l];
+	}
+	copy[l] = '\0';
+
+	return (copy);
+
+
+
+}
