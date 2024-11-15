c59a695f06cb0c33bc710150b6be520eaa173fa1
Kevinacostaa <kevinmag999@gmail.com>
2024-11-15 18:14:46 +0000

    3-dog_t

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -15,4 +15,18 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/**
+ * struct dog_t - dog_t
+ * @name: nombre
+ * @age: edad
+ * @owner: owner
+ */
+typedef struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
