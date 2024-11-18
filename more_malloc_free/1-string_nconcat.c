17d6179c8de834ec6893d1cfc805f7e229ee55d6
kevinacostaa <kevinmag999@gmail.com>
2024-11-18 14:51:37 +0000

    1-string_nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -33,7 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		n = long2;
 	}
 
-	ptr = malloc((long2 + n +1) * sizeof(char));
+	ptr = malloc((long1 + n +1) * sizeof(char));
 
 	if (ptr == NULL)
 	{
