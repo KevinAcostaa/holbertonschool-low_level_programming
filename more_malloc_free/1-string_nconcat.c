f22a5881a02b64054a25816ed022c147ba395605
kevinacostaa <kevinmag999@gmail.com>
2024-11-18 14:48:52 +0000

    1-string_nconcat

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * string_nconcat - function
+ * @s1: cadena
+ * @s2: cadena
+ * @n: n
+ * Return: ptr
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int long1, long2, i, j;
+	char *ptr;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	for (long1 = 0; s1[long1] != '\0'; long1++)
+	{
+	}
+	for (long2 = 0; s2[long2] != '\0'; long2++)
+	{
+	}
+
+	if (n >= long2)
+	{
+		n = long2;
+	}
+
+	ptr = malloc((long2 + n +1) * sizeof(char));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < long1; i++)
+	{
+		ptr[i] = s1[i];
+	}
+	for (j = 0; j < n; j++)
+	{
+		ptr[i + j] = s2[j];
+	}
+
+	ptr[i + j] = '\0';
+	return (ptr);
+}
