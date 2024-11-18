9fb9e185e2f240933625bf6f4f66c968ac14c651
kevinacostaa <kevinmag999@gmail.com>
2024-11-18 15:05:33 +0000

    2-calloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - function
+ * @nmemb; nmemb
+ * @size: size
+ * Return: ptr
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	void *ptr;
+	
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		((unsigned char *)ptr)[i] = 0;
+	}
+	return (ptr);
+
+}
