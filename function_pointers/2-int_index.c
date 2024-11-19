21977ce4e77e97127d088d575d5669ee0f63bf3a
kevinacostaa <kevinmag999@gmail.com>
2024-11-19 16:04:03 +0000

    2-int_index

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+/**
+ * int_index - function
+ * @array: array
+ * @size: size
+ * @cmp: puntero a function
+ * Return: always
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		cmp(array[i]);
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
