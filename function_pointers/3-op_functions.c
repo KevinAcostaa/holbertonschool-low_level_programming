1bce89ba389dec0ceb40c87269e0e4ad9a99a124
kevinacostaa <kevinmag999@gmail.com>
2024-11-19 18:36:18 +0000

    3-calc

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,31 +1,41 @@
 #include "3-calc.h"
+/**
+ * op_add - function sum
+ * op_sub - function res
+ * op_mul - function mul
+ * op_div - function div
+ * op_mod - function mod
+ * Return: always
+ * @a: a
+ * @b: b
+ */
 int op_add(int a, int b)
 {
-        return (a + b);
+	return (a + b);
 }
 int op_sub(int a, int b)
 {
-        return (a - b);
+	return (a - b);
 }
 int op_mul(int a, int b)
 {
-        return (a * b);
+	return (a * b);
 }
 int op_div(int a, int b)
 {
-        if (b == 0)
-        {
-                printf("Error\n");
-                exit(100);
-        }
-        return (a / b);
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
 }
 int op_mod(int a, int b)
 {
-        if (b == 0)
-        {
-                printf("Error\n");
-                exit(100);
-        }
-        return (a % b);
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
 }
