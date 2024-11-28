280d5d7a893d44da74900e115d2ace92a7b4f929
kevinacostaa <kevinmag999@gmail.com>
2024-11-28 14:03:59 +0000

    2-print_strings

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,41 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_string - function
+ * @separator: cadena
+ * @n: n
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int l;
+	char *str;
+	va_list p;
+
+	if (separator == NULL)
+	{
+		separator = "";
+	}
+
+	va_start(p, n);
+
+	for (l = 0; l < n; l++)
+	{
+		str = va_arg(p, char*);
+
+		if (str == NULL)
+		{
+			str = "(nill)";
+		}
+
+		if (l + 1 < n)
+		{
+			printf("%s%s", str, separator);
+		}
+		else
+		{
+			printf("%s", str);
+		}
+	}
+	va_end(p);
+	printf("\n");
+}
