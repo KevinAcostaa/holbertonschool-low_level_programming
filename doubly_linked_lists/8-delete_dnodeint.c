8704f94b9d3324b26bf308d11729fef6d20034e4
kevinacostaa <kevinmag999@gmail.com>
2024-12-08 20:52:20 +0000

    8-delete_dnodeint

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - function
+ * @head: head
+ * @index: index
+ *
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int count;
+
+	while (*head != NULL && count != index -1)
+	{
+		*head = (*head)->next;
+		count++;
+	}
+	return (count);
+}
